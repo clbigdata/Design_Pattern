79cca34c246091602ebf1bc48296d34c03cc6653
chenlong <clbigdata@163.com>
2020-09-04 16:33:54 +0800

    优化单例模式注释

--- Behavioral_Patterns/src/com/design/singleton/Thread_safe_Singleton_with_lazy_loading/DemoMultiThread.java
+++ Behavioral_Patterns/src/com/design/singleton/Thread_safe_Singleton_with_lazy_loading/DemoMultiThread.java
@@ -2,8 +2,13 @@ package com.design.singleton.Thread_safe_Singleton_with_lazy_loading;
 
 public class DemoMultiThread {
     public static void main(String[] args) {
+        /**
         System.out.println("If you see the same value, then singleton was reused (yay!)" + "\n" +
                 "If you see different values, then 2 singletons were created (booo!!)" + "\n\n" +
+                "RESULT:" + "\n");
+         **/
+       System.out.println("如果您看到相同的值，那么singleton被重用了（耶！）" + "\n" +
+                "如果看到不同的值，则创建了2个单例（booo！！" + "\n\n" +
                 "RESULT:" + "\n");
         Thread threadFoo = new Thread(new ThreadFoo());
         Thread threadBar = new Thread(new ThreadBar());
--- Behavioral_Patterns/src/com/design/singleton/Thread_safe_Singleton_with_lazy_loading/Singleton.java
+++ Behavioral_Patterns/src/com/design/singleton/Thread_safe_Singleton_with_lazy_loading/Singleton.java
@@ -3,6 +3,7 @@ package com.design.singleton.Thread_safe_Singleton_with_lazy_loading;
 public class Singleton {
     // The field must be declared volatile so that double check lock would work
     // correctly.
+    //保障线程间共享资源的可见性,防止重排序
     private static volatile Singleton instance;
 
     public String value;
@@ -12,23 +13,19 @@ public class Singleton {
     }
 
     public static Singleton getInstance(String value) {
-        // The approach taken here is called double-checked locking (DCL). It
-        // exists to prevent race condition between multiple threads that may
-        // attempt to get singleton instance at the same time, creating separate
-        // instances as a result.
-        //
-        // It may seem that having the `result` variable here is completely
-        // pointless. There is, however, a very important caveat when
-        // implementing double-checked locking in Java, which is solved by
-        // introducing this local variable.
-        //
-        // You can read more info DCL issues in Java here:
-        // https://refactoring.guru/java-dcl-issue
+
+        /**
+         * 执行双重检查是因为，如果多个线程同时了通过了第一次检查，并且其中一个线程首先通过了第二次检查并实例化了对象，
+         * 那么剩余通过了第一次检查的线程就不会再去实例化对象。
+         * 这样，除了初始化的时候会出现加锁的情况，后续的所有调用都会避免加锁而直接返回，解决了性能消耗的问题。
+         */
         Singleton result = instance;
+        //第一次检查，引用是否为null
         if (result != null) {
             return result;
         }
         synchronized(Singleton.class) {
+            //第二次检查，引用是否为null
             if (instance == null) {
                 instance = new Singleton(value);
             }
